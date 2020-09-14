82a12bf917507e0095e38b33d19c3e30d7c100a2
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 01:30:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 def prime?(number)
   start = 2
   if number > 1
-    puts 
+    range = (start..number-1).to_a
+    range.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
