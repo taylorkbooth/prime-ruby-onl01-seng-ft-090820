2e0822d6f6ba34a43c35a49763145e5655a851a6
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 01:24:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
-require 'prime'
+# require 'prime'
 
-num1 = 100
-num2 = 17
-num3 = 90
-num4 = 29
+# num1 = 100
+# num2 = 17
+# num3 = 90
+# num4 = 29
 
-def prime_numbers
-  puts num1.prime?
-  puts num2.prime?
-  puts num3.prime?
-  puts num4.prime?
+# def prime_numbers
+#   puts num1.prime?
+#   puts num2.prime?
+#   puts num3.prime?
+#   puts num4.prime?
 
-end
+# end
